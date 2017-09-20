057dfb53a89e4357319b2907dd1d578967f8d2cf
Trevor Brooks <trevorism@gmail.com>
2017-09-20 18:59:12 -0400

    Update wrapper and dependencies

--- src/main/groovy/com/trevorism/gcloud/webapi/controller/MailController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/MailController.groovy
@@ -7,18 +7,20 @@ import javax.ws.rs.Consumes
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.core.MediaType
+import java.util.logging.Logger
 
 /**
  * @author tbrooks
  */
 @Path("/mail")
 class MailController {
-
+    private static final Logger log = Logger.getLogger(MailController.class.name)
     private SendMailService mailService = new SendMailService()
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     boolean sendMail(Mail mail) {
+        log.info("Sending email to ${mail?.recipients}")
         mailService.sendMail(mail)
     }
 
