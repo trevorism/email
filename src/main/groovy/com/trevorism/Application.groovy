f95ea77b5eeea0596c13b5e2cd27cfa1eaaa0bc1
Trevor Brooks <trevorism@gmail.com>
2024-03-11 00:43:04 -0400

    Bump to version 1.0.1

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -10,9 +10,9 @@ import org.slf4j.LoggerFactory
 
 @OpenAPIDefinition(
         info = @Info(
-                title = "Schedule",
-                version = "1.3.0",
-                description = "Perform actions on a cadence",
+                title = "Email",
+                version = "1.0.1",
+                description = "Send emails",
                 contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
 )
